eb2fa8bd0ea98445cb720c1c99dfdf822d9348a1
Serbirial <gabesummers0@gmail.com>
2022-11-13 11:36:03 -0500

    Create main fn

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import beautifuldiscord.app as bd
 from argparse import ArgumentParser
 
-# parse CLI arguments
-parser = ArgumentParser('discord-spotify-bypass')
-parser.add_argument('--revert', action='store_false',
-  help='undo script injection, restore original discord app.asar')
-args = parser.parse_args()
+def main():
+  # parse CLI arguments
+  parser = ArgumentParser('discord-spotify-bypass')
+  parser.add_argument('--revert', action='store_false',
+    help='undo script injection, restore original discord app.asar')
+  args = parser.parse_args()
 
-print(args.revert)
+  # open discord process
+  discord: bd.DiscordProcess
+  try:
+    discord = bd.discord_process()
+  except Exception as e:
+    print(e)
+    return
 
-discord: bd.DiscordProcess
+  if args.revert:
+    bd.revert_changes(discord)
+    return
 
-try:
-  discord = bd.discord_process()
-except Exception as e:
-  print(e)
-  exit(1)
+main()
